986a89acb5a011d898d9457882171772b6789719
AbdallahEid <abdallaheidm@gmail.com>
2017-10-03 23:54:26 +0200

    Commits th fininshed project

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,7 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBOutlet var slider: UISlider!
+
+    @IBAction func sliderChanged(_ sender: Any) {
+        
+        table.reloadData()
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 50
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String( (Int(slider.value * 10)) * (indexPath.row + 1) )
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
